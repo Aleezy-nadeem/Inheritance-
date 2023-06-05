f70c6bc6f260b70b466f00e13763255bd3dddd69
Aleezy-nadeem <135522205+Aleezy-nadeem@users.noreply.github.com>
2023-06-05 12:58:03 +0500

    Add files via upload

---
+++ inheritence practice.cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+using namespace std;
+ class teacher{
+ 	public:
+ 	string name;
+ 	int id;
+ 	
+ 	
+ 	
+ };
+ class student:public teacher{   
+ public:
+ void teach(){
+ }
+ 
+ };
+ int maain() {
+ 	teacher t1;
+ teacher t1.name= "aliza";
+ 
+ }
